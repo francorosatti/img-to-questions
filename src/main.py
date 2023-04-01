46a0441d4f8804b555f66f9cd7024432f83be9ae
franrosattiLS <franco.rosatti@livestories.com>
2023-04-01 16:01:26 -0300

    Add scaling with opencv and argument to decide the scale

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import pytesseract
-
+import cv2
 from utils.arguments import parse_arguments
 
 
@@ -15,6 +15,19 @@ def run(filename):
     print(text)
 
 
+# For images too small, we can enlarge using openCV
+def run2(filename, factor=1):
+    # Read image from which text needs to be extracted
+    image = cv2.imread(filename)
+    image = cv2.resize(image, None, fx=factor, fy=factor)
+
+    # Run tesseract OCR on image
+    text = pytesseract.image_to_string(image)
+
+    # Print the extracted text
+    print(text)
+
+
 if __name__ == "__main__":
-    filename = parse_arguments()
-    run(filename)
+    filename, scale = parse_arguments()
+    run2(filename, scale)
--- src/utils/arguments.py
+++ src/utils/arguments.py
@@ -1,15 +1,24 @@
 import argparse
 
 
-def validate_arguments(args):
-    if args.filename is None:
-        exit(f"Filename argument --filename is required.")
+def int_range(minimum, maximum):
+    def range_checker(arg):
+        try:
+            i = int(arg)
+        except ValueError:
+            raise argparse.ArgumentTypeError("must be an intetger")
+        if i < minimum or i > maximum:
+            raise argparse.ArgumentTypeError(f"must be in range [{str(minimum)}, {str(maximum)}]")
+        return i
+
+    return range_checker
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename', dest='filename', required=False, default="test_data/test.png", type=str,
                         help='Path of the image file')
+    parser.add_argument('-s', '--scale', dest='scale', required=False, default=1, type=int_range(1, 5),
+                        help='Scale factor to enlarge image')
     args = parser.parse_args()
-    validate_arguments(args)
-    return args.filename
+    return args.filename, args.scale
